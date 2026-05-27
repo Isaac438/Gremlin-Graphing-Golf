9ecc59bfbfb4c68dc1b4a04c07a1ffc062396890
Isaac <isaacflenner@icloud.com>
2026-05-26 20:18:57 -0400

    added land detection

--- Game Files (From Godot)/scenes/ball.gd
+++ Game Files (From Godot)/scenes/ball.gd
@@ -3,13 +3,15 @@ extends Node2D
 var t := 0.0
 var speed := 0.5
 var moving := false
-
+@onready var area: Area2D = $Area2D
 var expression := Expression.new()
 var expression_ready := false
 
 func _ready():
 	set_expression(global.function)
-
+	area.area_entered.connect(_on_area_entered)
+	area.body_entered.connect(_on_area_entered)
+	
 func set_expression(expr_text: String):
 	var err = expression.parse(expr_text, ["x"])
 	expression_ready = (err == OK)
@@ -17,6 +19,14 @@ func set_expression(expr_text: String):
 func start():
 	t = 0.0
 	moving = true
+
+func _on_area_entered(area):
+	if area.is_in_group("Hole"):
+		pass
+	else:
+		moving = false
+		global_position = Vector2(0,0)
+		print("bad")
 
 func _process(delta):
 	if !moving or !expression_ready:
